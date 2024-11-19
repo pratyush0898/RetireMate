16f8b412a77c1cfed505c574b9c1c680fe30a732
Pratyush0898 <priyankarangwa@gmail.com>
2024-11-19 13:22:07 +0530

    Create Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import './App.css'
 import NavBar from './Components/NavBar/NavBar'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   return (
     <>
       <NavBar />
+      <br />
+      <Footer />
     </>
   )
 }
